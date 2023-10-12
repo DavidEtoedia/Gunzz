8c8ec1bd287768b061f30c635e14fc31c7a8e1ca
DavidEtoedia <etoedia@gmail.com>
2023-10-12 10:38:09 -0700

    Initial Commit

---
+++ Gunzz/GunzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  GunzzApp.swift
+//  Gunzz
+//
+//  Created by Inyene Etoedia on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GunzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
